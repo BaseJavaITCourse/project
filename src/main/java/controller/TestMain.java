f805adab96de3fca5d8b115e8c2bb9664ccfcee6
AnnaKovalek <anna.kovalek@gmail.com>
2015-04-28 12:55:42 +0300

    add

---
+++ src/main/java/controller/TestMain.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by admin on 26.04.2015.
+ */
+public class TestMain {
+
+    private static Logger LOG = Logger.getLogger(TestMain.class);
+
+    public static void main(String[] args) {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("sdgdgsd");
+        }
+
+        LOG.info("sdgdgsd");
+
+
+    }
+
+}
